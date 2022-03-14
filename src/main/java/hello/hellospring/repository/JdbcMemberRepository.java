3a69874f5dfcc216cb50b27151ef44bc44aa9bff
jlal1226 <jlal1226@naver.com>
2022-03-14 20:03:40 +0900

    스프링 통합 테스트

--- src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
+++ src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
@@ -20,7 +20,7 @@ public class JdbcMemberRepository implements MemberRepository{
 
     @Override
     public Member save(Member member) {
-        String sql = "insert into member(name) value (?)";
+        String sql = "insert into member(name) values (?)";
         Connection conn = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
